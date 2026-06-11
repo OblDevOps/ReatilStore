ec5cd9e92e91cce6ae184b750a70fa7d3e149909
JoacoGDev <joacogilp@gmail.com>
2026-06-11 14:32:51 -0300

    Feat: agregar environments con archivo terraform.tf

---
+++ environments/dev/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "retailstore-tfstate"
+    key     = "dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
