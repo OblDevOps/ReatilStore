de72f4bdcc9100bffbff3bbc81b3822b559cb252
JoacoGDev <joacogilp@gmail.com>
2026-06-15 15:09:31 -0300

    Feat: Se agrega tabla de rutas publicas, y su asociación

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -42,3 +42,26 @@ resource "aws_subnet" "private_subnet" {
     Environment = var.environment
   }
 }
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name        = "${var.vpc_name}-public-route-table"
+    Environment = var.environment
+  }
+}
+
+//Implementa la ruta que dirige a internet
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+//Asocia la tabla a las subnets publicas
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
