b4c36cee29eef8a350db756a1e79052a9715cc32
JoacoGDev <joacogilp@gmail.com>
2026-06-15 14:09:53 -0300

    Feat: implementa subnets públicas y privadas

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -16,3 +16,29 @@ resource "aws_internet_gateway" "igw" {
     Environment = var.environment
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name        = "${var.vpc_name}-public-${count.index + 1}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  map_public_ip_on_launch = false //Evita asignar ip pública al crear subnet
+
+  tags = {
+    Name        = "${var.vpc_name}-private-${count.index + 1}"
+    Environment = var.environment
+  }
+}
