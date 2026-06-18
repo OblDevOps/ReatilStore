c8614b4f244dbd1229d2eeec2ac883e797ece6c4
JoacoGDev <joacogilp@gmail.com>
2026-06-18 14:40:56 -0300

    Feat: agrega modulo para el servicio de ui

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -19,3 +19,31 @@ module "ecs" {
   cluster_name = var.cluster_name
   environment  = var.environment
 }
+
+# modulo para el servicio de ui.
+module "service_ui" {
+  source = "../../modules/ecs_service"
+
+  service_name = "ui"
+  environment  = var.environment
+
+  # de la red
+  vpc_id             = module.network.vpc_id
+  public_subnet_ids  = module.network.public_subnet_ids
+  private_subnet_ids = module.network.private_subnet_ids
+
+  #  cluster
+  cluster_id = module.ecs.cluster_id
+
+
+  container_image = "${module.ecr.repository_urls["ui"]}:latest"
+
+  # configuración del contenedor
+  container_port = 8080
+  cpu            = 256
+  memory         = 512
+  desired_count  = 1
+
+  # precisamos el rol de ejecución (LabRole en el Learner Lab)
+  execution_role_arn = var.execution_role_arn
+}
