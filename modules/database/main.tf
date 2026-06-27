68a690adbdfbe8d159cf5bc6a4d6b8bd354adb82
JoacoGDev <joacogilp@gmail.com>
2026-06-27 20:54:33 -0300

    Fix: se cambia for_each a count

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -65,10 +65,10 @@ resource "aws_efs_file_system" "postgres" {
 
 # Mount target por cada subnet privada para que ECS pueda montar EFS desde cualquier AZ
 resource "aws_efs_mount_target" "postgres" {
-  for_each = toset(var.private_subnet_ids)
+  count = length(var.private_subnet_ids)
 
   file_system_id  = aws_efs_file_system.postgres.id
-  subnet_id       = each.value
+  subnet_id       = var.private_subnet_ids[count.index]
   security_groups = [aws_security_group.efs.id]
 }
 
